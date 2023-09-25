19164a9892d61c64fd00c8100b5301d59e1e0066
MIAN MOHAMMAD RASSEL <145997251+mmrassel@users.noreply.github.com>
2023-09-25 20:41:14 +0600

    Update numericCons.cpp
    updating comment

--- numericCons.cpp
+++ numericCons.cpp
@@ -7,7 +7,7 @@ int main()
     string s;
     cin>>s;
     int count=0;
-
+//checking constant
     for(int i=0; i<s.size(); i++)
     {
         if((s[i]=='0') || (s[i]=='1') || (s[i]=='2') || (s[i]=='3') || (s[i]=='4') || (s[i]=='5') || (s[i]=='6') || (s[i]=='7') || (s[i]=='8') || (s[i]=='9') )
